de29d1172dfe00709795538ae3b9f7c7d9022f4a
Pranaw-dev <juyal.pranaw@gmail.com>
2025-03-19 23:53:09 +0530

    new java code

---
+++ newjava.java
@@ -0,0 +1,12 @@
+public class new {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println(Integer.toString(i));
+
+        String s = "pranaw";
+        String s1 = "pranaw_op";
+        System.out.println(s+" \nclear new feature");
+        System.out.println(s1+" \nclear new feature 1");
+    }    
+
+}
